162969f98147b275dd699b244e46289227e59102
Sgoldik <sgoldik@mail.ru>
2019-12-29 22:52:10 +0700

    0.0.8

--- index2.js
+++
@@ -1,7 +0,0 @@
-let decrybe = require("./index")
-
-let nodeUrl = "https://testnodes.wavesnodes.com"
-let seed = "melody eye stock ostrich camera talk unlock royal insane pipe step squeeze";
-
-//decrybe.acceptWork("dddd", false, nodeUrl, seed)
-decrybe.signUp("Help", nodeUrl, seed)
