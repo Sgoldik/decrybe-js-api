50c87dd8afce9bb01f281863e1d7fab9eb65035b
Sgoldik <sgoldik@mail.ru>
2019-12-29 21:23:45 +0700

    0.0.5

--- index.ts
+++
@@ -1,9 +0,0 @@
-export { 
-acceptWork,
-createTask,
-signUp,
-taskUpdate,
-userUpdate,
-hireFreelancer,
-reportCompleteTask,
- } from './fuctions'
---
+++ index2.js
@@ -0,0 +1,5 @@
+let decrybe = require("./index")
+
+let nodeUrl = "https://testnodes.wavesnodes.com"
+
+decrybe.acceptWork("dddd", false, nodeUrl)
